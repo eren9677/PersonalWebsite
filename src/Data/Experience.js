29c77d0e1d769d619b9b8e8db2959bd02d3ae803
Eren Kızılırmak <109414946+eren9677@users.noreply.github.com>
2025-07-03 13:44:57 +0300

    introduced new line elements for experience js <br> tags

--- src/Data/Experience.js
+++ src/Data/Experience.js
@@ -10,30 +10,30 @@ const experience = [
       year: 'Sep 2024 - Jun 2025',
       description:
         `
-        • Conducted machine learning research on obesity prediction using artificial intelligence techniques
-under the supervision of Dr. Muhammed Burak Alver.
+• Conducted machine learning research on obesity prediction using artificial intelligence techniques<br>
+under the supervision of Dr. Muhammed Burak Alver.<br>
 • Developed data augmentation methods to address limited healthcare data challenges and improve
-model performances by up to 12.5%.
+model performances by up to 12.5%.<br>
 • Presented research findings at SIU 2025, Turkey's leading signal processing conference and the
 work was accepted for publication in IEEE Xplore proceedings. Applied Pytorch, Ydata based
-machine learning frameworks throughout the research process.
+machine learning frameworks throughout the research process.<br>
         `
     },
   
     {
       title: 'Teknoloji Liderleri',
       image: teknolojiliderleri,
-      position: 'ML R&D Intern',
+      position: 'Volunteer Engineer',
       year: 'jul - Aug 2024',
       description:
         `
-      Assisted with special events and programs.
-• Attended seminars on funding mechanisms for sustainable tech projects.
-• Explored robotics (Python/ROS) and drone systems.
-• Developed a SwiftUI based Mini Flag Guessing Game as part of application workshop.
-• I joined meetings with tech mentors. We shared ideas for EU projects that need funding.
+• Assisted with special events and programs. <br>
+• Attended seminars on funding mechanisms for sustainable tech projects.<br>
+• Explored robotics (Python/ROS) and drone systems.<br>
+• Developed a SwiftUI based Mini Flag Guessing Game as part of application workshop.<br>
+• I joined meetings with tech mentors. We shared ideas for EU projects that need funding.<br>
 • Helped run our team's booth and talked to students and visitors about supporting Teknofest
-projects.
+projects.<br>
         `
     },
     {
@@ -43,10 +43,12 @@ projects.
       year: 'May - Aug 2023',
       description:
         `
-        Collaborated in a team to design and experiment on model improvement techniques. Applied principal
-component analysis (PCA) to improve regression model’s accuracy by 4%.(Sklearn, Python, Scipy).
-Applied the pmdarima, prophet and tensorflow libraries to perform exploratory data analysis (EDA) on time
-series data. Authored technical reports for improved ML models (Matplotlib, Excel, LaTeX).
+• Collaborated in a team to design and experiment on model improvement techniques.<br>
+• Applied principal component analysis (PCA) to improve regression model's accuracy by 4%.(Sklearn,
+Python, Scipy).<br>
+• Applied the pmdarima, prophet and tensorflow libraries to perform exploratory data analysis (EDA) on
+time series data.<br>
+• Authored technical reports for improved ML models (Matplotlib, Excel, LaTeX).<br>
         `
     }
   ]
